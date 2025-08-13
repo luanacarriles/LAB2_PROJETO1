978136c7323bf5d193c725d6a3ea7947884fffa9
Luana Carriles Alves <C3007876@fgv.edu.br>
2025-08-13 15:53:18 -0300

    projeto final

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-def cumprimento(texto):
-    return f"Olá, {texto}"
-print(cumprimento("Luana Carriles"))
+import random 
+
+def media_7_numero():
+    mumeros = [random.randit(1, 100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    return numeros, media
+
+numeros_sorteados, media = media_7_numero()
+print('Numeros sorteados:', numeros_sorteados)
+print(f'Média dos 7 números sorteados: {media:.2f}')
