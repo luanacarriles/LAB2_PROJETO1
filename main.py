9d77ebe5e918adf84455fcb96e21fcad1daf1ecd
Luana Carriles Alves <C3007876@fgv.edu.br>
2025-08-13 14:34:55 -0300

    teste3

---
+++ main.py
@@ -0,0 +1,8 @@
+def cumprimento(texto):
+    return f"Olá {texto}"
+print( cumprimento("Luana Carriles"))
+import random
+def media_7_numeros():
+    numeros= [random.randint(1,100) for _ in range(7)]
+    media= sum(numeros) / len(numeros)
+    return numeros, media 
